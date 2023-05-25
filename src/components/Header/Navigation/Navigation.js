eef47c3c15f432a8aba80e3c6c9ff5e2a4bceba0
Tigerstyle29 <tvartanyan29@yandex.ru>
2023-05-25 15:15:35 +0300

    Добавил бургер меню

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components'
+// icons for mobile
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+// hook 
+import { useState } from 'react'
 
 // TODO: Примерно на 870px (разрешение) сделать бургер меню
 const Navigation = () => {
+    const [nav, setNav] = useState(false);
     return (
         <NavList>
+            { nav ? [NavList, active].join(' ') : [NavList] } 
             <NavEl>Обо мне</NavEl>
             <Line/>
 
@@ -17,6 +23,11 @@ const Navigation = () => {
             <Line/>
 
             <NavEl>Блог</NavEl>
+            
+            <AiOutlineMenuStyled size={25} onClick={() => setNav(!nav)}>  
+                {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu/>}
+            </AiOutlineMenuStyled>
+            
         </NavList>
     )
 }
@@ -27,18 +38,56 @@ const NavList = styled.div`
     height: inherit;
     color: #FFFFFF;
     font-size: 16px;
-`
+
+    @media (max-width: 870px) {
+        display: flex;
+        flex-direction: column;
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        // высота на все окно 
+        height: 100vh;
+        color: black;
+        background-color: #f8f8f8;
+        z-index: 10;
+        transition: left 1s;
+    }
+        &.active {
+              left: 0;
+          }
+` 
 
 const Line = styled.span`
     border-right: #FFFFFF solid 1px; 
     height: 25%;
     margin: 0 15px;
+
+    @media (max-width: 870px) {
+        display: none;
+    }
 `
 
 const NavEl = styled.p`
     &:hover {
         color: #609f89;
         cursor: pointer;
+    }
+`
+// бургер меню 
+
+const AiOutlineMenuStyled = styled(AiOutlineMenu)`
+    display: none;
+    
+    @media  (max-width: 870px) {
+        display: block;
+        position: absolute;
+        right: 10px;
+        top: 29px;
+        cursor: pointer;
+        z-index: 10;
     }
 `
 
