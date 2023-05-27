7c6e149ad97ad89e13fbfec64d197d02fdb661b8
Tigerstyle29 <tvartanyan29@yandex.ru>
2023-05-27 19:43:28 +0300

    Добавлено бургер меню, но с костылём

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,6 +13,7 @@ const Header = () => {
                 <Navigation />
                 <Phone />
                 <Social />
+                <div></div>
             </HeaderStyled>
         </Background>
     )
