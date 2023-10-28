452924a2935d343a71be7c3a486705ef706be0a9
Р34-Антон <r34-anton@monowork.pro>
2023-10-28 15:47:00 +0300

    Изменение подстроки в строке

--- src/components/Help/HelpCard.js
+++ src/components/Help/HelpCard.js
@@ -3,10 +3,14 @@ import styled from 'styled-components'
 
 function HelpCard({card}) {
     const {heading, text, id} = card
+
     return (
         <Wrapper key={id}>
             <Heading>
-                {heading}
+                <BackgroundFirstLetter>
+                    {heading[0]}
+                </BackgroundFirstLetter>
+                {heading.replace(heading[0], '')}
             </Heading>
             <Text>
                 {text}
@@ -23,9 +27,16 @@ const Wrapper = styled.div`
     width: 80%;
   }
 `
+
+const BackgroundFirstLetter = styled.span`
+  background-color: #6C5D9533;
+  padding-left: 35px;
+`
 const Heading = styled.h2`
 `
 const Text = styled.p`
+  padding-left: 35px;
+
 `
 
 export default HelpCard
